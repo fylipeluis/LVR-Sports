5aefc9eb73d9dd03cac90e7be299d53de78cf1b9
Luis <luisfylipe1802@gmail.com>
2025-08-15 16:13:31 -0300

    Mudanças de estylo e desultilização de header transparente

--- header.js
+++
@@ -1,9 +0,0 @@
-let header = document.getElementById('header')
-
-window.addEventListener('scroll', ()=>{
-    if(window.scrollY >= 200){
-        header.style.background = 'transparent'
-    } else {
-        header.style.background = 'rgb(64, 139, 208)'
-    }
-})
